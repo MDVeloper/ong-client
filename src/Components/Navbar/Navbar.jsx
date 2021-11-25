3ed020f94b8cc92efe3b50cabcb6c6026fe001b2
Ludmi <75680853+LudmilaB12@users.noreply.github.com>
2021-11-25 17:07:22 -0300

    Navbar lista, funcionando y con los estilos hechos, falta la font principal en el botón donar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "./LOGO.png";
+import styles from "./Navbar.module.css";
 
 export default function Navbar(){
 
     return(
-        <div>
+        <div className={styles.space}>
             <nav>
-                <ul>
+                <ul className={styles.navbar}>
                     <li>
                         <Link to="/about-us">About Us</Link>
                     </li>
@@ -22,7 +23,7 @@ export default function Navbar(){
                     </li>
                 </ul>
             </nav>
-            <button>Donar</button>
+            <button className={styles.button}>Donar</button>
             <Link to="/home"><img src={logo} alt=" "/></Link>
         </div>
     )

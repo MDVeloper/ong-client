47573520d4bdec5bdf499715be0a2c4dcd5a82dc
Ludmi <75680853+LudmilaB12@users.noreply.github.com>
2021-11-25 13:17:18 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux'; //Bindings from redux and React
-//import { store } from './store/store.js';
+import store  from './Store/Index'
+import Navbar from './Components/Navbar/Navbar.jsx';
 import './App.css';
 
 function App() {
 
   return (
-    /* <Provider store={store}>
+   <Provider store={store}>
       <BrowserRouter>
         <Switch>
-          <Route exact path='/' component={Start} />
+          <Route exact path='/navbar' component={Navbar} />
         </Switch> 
       </BrowserRouter>
-    </Provider> */
+    </Provider> 
     
-      <div className="App">
-        <h1>App</h1>
-      </div>
+      // <div className="App">
+      //   <h1>App</h1>
+      // </div>
       
   );
 }

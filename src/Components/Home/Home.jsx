8e5e30dcf47f923bc7a1dce15ecb443244717127
Jonathan Armesto <armestojona@gmail.com>
2021-12-02 09:22:50 -0300

    home y footer completos

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,38 +1,45 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import Button from '@mui/material/Button'
-import Fab from '@mui/material/Fab';
-import NavigationIcon from '@mui/icons-material/Navigation';
-import s from './Home.module.css'
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import Fab from "@mui/material/Fab";
+import NavigationIcon from "@mui/icons-material/Navigation";
+import s from "./Home.module.css";
 export default function Home() {
-
   return (
     <div>
-      
+      <section className={s.mainContainer}>
+        <div>
+          <Link to="/donations">
+            <Button
+              variant="contained"
+              size="large"
+              className={s.btn}
+              aria-label="add"
+            >
+              Apoya la causa
+            </Button>
+          </Link>
 
-        <section className={s.mainContainer}>
-           <div>
-           <Button variant="contained" size="large" className={s.btn}  aria-label="add">Apoya la causa</Button>
-            <Button variant="contained" size="large" className={s.btn} aria-label="add">Ser Voluntario</Button>
-           </div>
-           
-        </section>
-        <br/>
-        <div>
-            ACA VAN A IR LAS ESTADISTICAS
+          <Link to="/voluntariado">
+            <Button
+              variant="contained"
+              size="large"
+              className={s.btn}
+              aria-label="add"
+            >
+              Ser Voluntario
+            </Button>
+          </Link>
         </div>
-                <br/>
-        <div>
-            ACA VA A IR El CARROUSEL
-        </div>
+      </section>
+
       <section className={s.wrapperVolunteerDonation}>
         <div className={s.containerVolunteer}>
           <div className={s.containerVolunteerText}>
-            <h3>
-              Manos a la obra
-            </h3>
+            <h3>Manos a la obra</h3>
             <p>
-              Para poder ayudar no solamente es necesaria tu donacion sino que te podes sumar a nuestro equipo manos a la obra
+              Para poder ayudar no solamente es necesaria tu donacion sino que
+              te podes sumar a nuestro equipo manos a la obra
             </p>
           </div>
         </div>
@@ -41,35 +48,31 @@ export default function Home() {
           <div className={s.containerDonationText}>
             <h3>Entre todos</h3>
             <p>
-              Podes acercarte a cualquiera de nuestro puntos y acercarnos tu donacion para compartir tu estilo de vida
+              Podes acercarte a cualquiera de nuestro puntos y acercarnos tu
+              donacion para compartir tu estilo de vida
             </p>
           </div>
         </div>
       </section>
 
-<section className={s.containerQuestionAbout}>
-     <div>
-       
-        <Link to='/aboutUs'>
-        <Fab variant="extended" className={s.btn} aria-label="add">
-  <NavigationIcon sx={{ mr: 1 }} />
-  Sobre Nosotros
-</Fab>
-        </Link>
-    </div>
-    <div>
-       
-        <Fab variant="extended" className={s.btn} aria-label="add">
-  <NavigationIcon sx={{ mr: 1 }} />
-  Preguntas frecuentes
-</Fab>
-    </div>
-</section>
-   
-
-
-
-      
+      <section className={s.containerQuestionAbout}>
+        <div>
+          <Link to="/aboutUs">
+            <Fab variant="extended" className={s.btn} aria-label="add">
+              <NavigationIcon sx={{ mr: 1 }} />
+              Sobre Nosotros
+            </Fab>
+          </Link>
+        </div>
+        <div>
+          <Link to="/preguntasFrecuentes">
+            <Fab variant="extended" className={s.btn} aria-label="add">
+              <NavigationIcon sx={{ mr: 1 }} />
+              Preguntas frecuentes
+            </Fab>
+          </Link>
+        </div>
+      </section>
     </div>
   );
 }

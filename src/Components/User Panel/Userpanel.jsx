fc03486cc4980fa02c7969aaaa31e175463aaf4a
chicha099 <tdsserafin@gmail.com>
2021-12-05 23:30:29 -0300

    deploy profile

--- src/Components/User Panel/Userpanel.jsx
+++ src/Components/User Panel/Userpanel.jsx
@@ -32,7 +32,7 @@ export default function Userpanel(props) {
             </div>
 
             <VerticalTabs/>
-
+            
             
 
         
--- src/Components/User Panel/Vertical tabs.jsx
+++ src/Components/User Panel/Vertical tabs.jsx
@@ -5,17 +5,20 @@ import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { createTheme } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { getAllTransactions } from '../../Store/Actions/actionDonations';
+import { useEffect } from 'react';
+import style from "./UserPanel.module.css";
 
 const theme = createTheme({
   MuiTabs: {
-    
+
   }
 })
 
-
-
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
+
 
   return (
     <div
@@ -49,14 +52,20 @@ function a11yProps(index) {
 
 export default function VerticalTabs() {
   const [value, setValue] = React.useState(0);
+  const dispatch = useDispatch()
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+  const allTransactions = useSelector((state) => state.donations.allTransactions);
+
+  useEffect(() => {
+    dispatch(getAllTransactions());
+  }, [dispatch]);
 
   return (
     <Box
-      sx={{ flexGrow: 1, bgcolor: '#F3F3F3', display: 'flex', height: 500}}
+      sx={{ flexGrow: 1, bgcolor: '#F3F3F3', display: 'flex', height: 500 }}
     >
       <Tabs
         orientation="vertical"
@@ -64,7 +73,7 @@ export default function VerticalTabs() {
         value={value}
         onChange={handleChange}
         aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: 'divider'}}
+        sx={{ borderRight: 1, borderColor: 'divider' }}
       >
         <Tab label="Donaciones" {...a11yProps(0)} />
         <Tab label="Proyectos" {...a11yProps(1)} />
@@ -73,6 +82,18 @@ export default function VerticalTabs() {
       </Tabs>
       <TabPanel value={value} index={0}>
         Información de las donaciones
+        {allTransactions && allTransactions.map(t => {
+          return (
+            <div className={style.transactionDiv}>
+              <h6>{t.id}</h6>
+              <h6>{t.email}</h6>
+              <h6>${t.amount}</h6>
+              <h6>{t.date.slice(0, 10)}</h6>
+              <h6>{t.paymentMethod}</h6>
+              <h6>{t.status}</h6>
+            </div>
+          )
+        })}
       </TabPanel>
       <TabPanel value={value} index={1}>
         Toda la información respectiva de los proyectos etc

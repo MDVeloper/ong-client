3425142f6db9523bb6898e2c5a797453e3e0bbeb
Ludmi <75680853+LudmilaB12@users.noreply.github.com>
2021-12-13 17:39:31 -0300

    Navbar %100 responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import logo from "./LOGO.png";
 import logo2 from "./LOGO2.png";
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+
+    const [clicked, setClicked ] = useState(false);
+
+    const showMenu = () => setClicked(!clicked)
+ 
+
+
     return (
         <div className={styles.space}>
 
@@ -15,23 +22,29 @@ export default function Navbar() {
                 <button className={styles.button}>Donar</button>
             </Link>
 
-                <div className={styles.iconMenu}>
-                    <i class="fas fa-bars"></i>
-                 </div>
             <nav>
-                <ul className={styles.navbar}>
+                <div className={styles.iconMenu}>
+                  
+                      <Link to="#" onClick={showMenu}>
+
+                       <i class="fas fa-bars"></i>
+                      </Link>
+                       
+                 </div>
+
+                <ul className={clicked?  styles.navbarActive : styles.navbar}>
                     <li>
-                        <Link to="/aboutUs">About Us</Link>
+                        <Link to="/aboutUs" onClick={showMenu}>About Us</Link>
                     </li>
                     <li>
-                        <Link to="/noticias">Noticias</Link>
+                        <Link to="/noticias" onClick={showMenu}>Noticias</Link>
                     </li>
                     <li>
-                        <Link to="/proyectos">Proyectos</Link>
+                        <Link to="/proyectos" onClick={showMenu}>Proyectos</Link>
                     </li>
 
                     <li>
-                        {localStorage.getItem("token") ? <Link to="/users">Mi Perfil</Link> : <Link to="/login">Log In/Register</Link>}
+                        {localStorage.getItem("token") ? <Link to="/users" onClick={showMenu}>Mi Perfil</Link> : <Link to="/login" onClick={showMenu}>Log In/Register</Link>}
                     </li>
 
                     {localStorage.getItem("token") ? <li>

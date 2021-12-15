d0d5ada5a9501e592446dfae1cef357c9fb0f0a5
chicha099 <tdsserafin@gmail.com>
2021-12-15 11:16:19 -0300

    action project

--- src/Store/Actions/actionGetProjects.js
+++ src/Store/Actions/actionGetProjects.js
@@ -6,7 +6,7 @@ export function getProject() {
     return async function(dispatch) {
         try {
             // Hablitar cuando se sepa la ruta que me trae las cosas del back
-            const projects = await axios.get(`/articles/?category=Projects`)
+            const projects = await axios.get(`/articles?category=Projects`)
 
             dispatch({
                 type : "GET_PROJECT",

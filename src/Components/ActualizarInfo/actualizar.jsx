04a545be3c35063faa1c47f9bd578e4ad0be63c9
chicha099 <tdsserafin@gmail.com>
2021-12-15 11:00:48 -0300

    cambios actualizar

--- src/Components/ActualizarInfo/actualizar.jsx
+++ src/Components/ActualizarInfo/actualizar.jsx
@@ -7,7 +7,7 @@ import moment from "moment";
 
 import { Button, TextField } from "@mui/material";
 import { FcGoogle } from "react-icons/fc";
-import Styles from "./Actualizar.module.css"
+import Styles from "./actualizar.module.css"
 import Loading from '../Loading/Loading'
 
 import jwt_decode from "jwt-decode"

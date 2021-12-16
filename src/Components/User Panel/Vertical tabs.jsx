391fe2a3bb2ad1e46ce60bee25f325de353b0b33
Ludmi <75680853+LudmilaB12@users.noreply.github.com>
2021-12-15 21:55:06 -0300

    Panel Usuario terminado

--- src/Components/User Panel/Vertical tabs.jsx
+++ src/Components/User Panel/Vertical tabs.jsx
@@ -16,6 +16,18 @@ import { useState } from 'react';
 import styles from "./UserPanel.module.css"
 import { getProject } from "../../Store/Actions/actionGetProjects.js"
 import { getAllTransactions } from '../../Store/Actions/actionDonations';
+import { getCategories } from '../../Store/Actions/actionArticles';
+
+
+///tablas mui
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 
@@ -78,6 +90,23 @@ export default function VerticalTabs({ history }) {
 
   //----------------------------------------------------------
 
+  //LO QUE NECESITO PARA CURSOS
+
+  const allArt = useSelector((state) => state.articles.categories);
+
+  useEffect(() => {
+    dispatch(getCategories("Course"))
+    }, [])
+
+
+  //-----------------------------------------------------------
+
+  //LO QUE NECESITO PARA NOTICIAS
+
+  let news = useSelector((state) => state.new.news);
+
+  //-----------------------------------------------------------
+
   if (localStorage.getItem("token") && userid === ""){
       const data = localStorage.getItem("token")
       setuserid(jwt_decode(data))
@@ -102,7 +131,7 @@ export default function VerticalTabs({ history }) {
 
   return (
     <Box
-      sx={{ flexGrow: 1, bgcolor: '#F3F3F3', display: 'flex', height: 500 } } className={style.userContainer}
+      sx={{ flexGrow: 1, bgcolor: '#F3F3F3', display: 'flex', height: 600 } } className={style.userContainer}
     >
       <Tabs
         orientation="vertical"
@@ -123,32 +152,91 @@ export default function VerticalTabs({ history }) {
         <h2> Información de las donaciones </h2> 
 
         {
-          userinfo.privilege === "Admin" ?
-          allDonations.map( t => {
-            return (
-              <div>
-                <h6>{t.id}</h6>
-                <h6>{t.email}</h6>
-                <h6>${t.amount}</h6>
-                <h6>{t.date.slice(0, 10)}</h6>
-                <h6>{t.paymentMethod}</h6>
-                <h6>{t.status}</h6>
-              </div>
-            )
-          }) :
-          userinfo.donatios ? userinfo.donations.map(t => {
-            return (
-              <div key={t.id} className={style.transactionDiv}>
-                <p>Acá podrás ver un historial de tus donaciones hechas</p>
-                <h6>{t.id}</h6>
-                <h6>{t.email}</h6>
-                <h6>${t.amount}</h6>
-                <h6>{t.date.slice(0, 10)}</h6>
-                <h6>{t.paymentMethod}</h6>
-                <h6>{t.status}</h6>
-              </div>
-            )
-          }) :
+          userinfo.privilege === "Admin" ?    
+          <TableContainer component={Paper} sx={{ maxHeight: 500 }}>
+          <Table sx={{ maxWidth: 1700, overflow: 'scroll'}} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>id</TableCell>
+                
+                <TableCell align="right">E-mail</TableCell>
+                <TableCell align="right">Cantidad</TableCell>
+                <TableCell align="right">Fecha</TableCell>
+                <TableCell align="right">Método de pago</TableCell>
+                <TableCell align="right">Estado</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {allDonations.map((donations) => (
+                <TableRow
+                  key={donations.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {donations.id}
+                  </TableCell>
+                  <TableCell align="right" sx={{ maxWidth: 900, overflow: 'hidden'}} >{donations.email}</TableCell>
+                  <TableCell align="right">{donations.date.slice(0, 10)}</TableCell>
+                  <TableCell align="right">{donations.paymentMethod}</TableCell>
+                  <TableCell align="right">{donations.status}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+
+
+
+
+          // allDonations.map( t => {
+          //   return (
+          //     <div>
+          //       <h6>{t.id}</h6>
+          //       <h6>{t.email}</h6>
+          //       <h6>${t.amount}</h6>
+          //       <h6>{t.date.slice(0, 10)}</h6>
+          //       <h6>{t.paymentMethod}</h6>
+          //       <h6>{t.status}</h6>
+          //     </div>
+          //   )
+          // }) 
+          :
+          userinfo.donatios ?
+
+          <TableContainer component={Paper} sx={{ maxHeight: 500 }}>
+          <Table sx={{ maxWidth: 1700, overflow: 'scroll'}} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>id</TableCell>
+                
+                <TableCell align="right">E-mail</TableCell>
+                <TableCell align="right">Cantidad</TableCell>
+                <TableCell align="right">Fecha</TableCell>
+                <TableCell align="right">Método de pago</TableCell>
+                <TableCell align="right">Estado</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {allDonations.map((donations) => (
+                <TableRow
+                  key={donations.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {donations.id}
+                  </TableCell>
+                  <TableCell align="right" sx={{ maxWidth: 900, overflow: 'hidden'}} >{donations.email}</TableCell>
+                  <TableCell align="right">{donations.date.slice(0, 10)}</TableCell>
+                  <TableCell align="right">{donations.paymentMethod}</TableCell>
+                  <TableCell align="right">{donations.status}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+           :
            <div>
              <p>Acá podrás ver un historial de tus donaciones hechas</p>
              <h4>De momento no has hecho ninguna donacion</h4>
@@ -161,37 +249,107 @@ export default function VerticalTabs({ history }) {
 
         {
           userinfo.privilege === "Admin" ? 
-          projects.map( p => {
-            return(
-              <div>
-                <h5>{p.title}</h5>
-                <h5>{p.img}</h5>
-                <h5>{p.description}</h5>
-                <h5>{p.category}</h5>
-                <h5>{p.status}</h5>
-                <Link to={`/backoffice/form/${p.id}`}>Editar proyecto</Link>
-              </div>
-            )
-          }) : <p>Para votar por los proyectos que mas te gusten haciendo click <a href='/proyectos'>aquí</a></p>
+          <TableContainer component={Paper} sx={{ maxHeight: 500 }}>
+      <Table sx={{ maxWidth: 1700, overflow: 'scroll'}} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Título</TableCell>
+            
+            <TableCell align="right">Descripción</TableCell>
+            <TableCell align="right">Categoría</TableCell>
+            <TableCell align="right">Status</TableCell>
+            <TableCell align="right">Modificar</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {projects.map((project) => (
+            <TableRow
+              key={project.title}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {project.title}
+              </TableCell>
+              <TableCell align="right" sx={{ maxWidth: 900, overflow: 'hidden'}} >{project.description}</TableCell>
+              <TableCell align="right">{project.category}</TableCell>
+              <TableCell align="right">{project.status}</TableCell>
+              <TableCell align="right"><Link to={`/backoffice/form/${project.id}`}>Editar proyecto</Link></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+                  : <p>Para votar por los proyectos que mas te gusten haciendo click <a href='/proyectos'>aquí</a></p>
 
         }
         
       </TabPanel>
       <TabPanel value={value} index={2}>
-        <h2>Toda la información respectiva de los cursos</h2>
+        <h2>Toda la información de los cursos</h2>
         {
           userinfo.privilege === "Admin"? 
-          <h2>info de admin</h2>
+          <TableContainer component={Paper} sx={{ maxHeight: 500 }}>
+          <Table sx={{ maxWidth: 1700, overflow: 'scroll'}} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>id</TableCell>
+                
+                <TableCell align="right">Título</TableCell>
+                <TableCell align="right">Descripción</TableCell>
+                <TableCell align="right">Modificar</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {allArt.map((c) => (
+                <TableRow
+                  key={c.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                      {c.id}
+                  </TableCell>
+                  <TableCell align="right" sx={{ maxWidth: 900, overflow: 'hidden'}} >{c.title}</TableCell>
+                  <TableCell align="right">{c.description}</TableCell>
+                  <TableCell align="right"><Link to={`/backoffice/form/${c.id}`}>Editar curso</Link></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
           :
-          userinfo.articles? userinfo.articles.map(t => {
-             return (
-                 <div key={t.id} className={style.transactionDiv}>
-                  <h6>{t.id}</h6>
-                  <h6>{t.title}</h6>
-                   <h6>{t.description}</h6>
-                 </div>
-               )
-             }) : <h4>¿Aún no te inscribiste a ningun curso? Conocé todos los que hay disponibles <a href='/curse'>aquí</a>  </h4>
+          userinfo.articles? 
+
+          <TableContainer component={Paper} sx={{ maxHeight: 500 }}>
+          <Table sx={{ maxWidth: 1700, overflow: 'scroll'}} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>id</TableCell>
+                
+                <TableCell align="right">Título</TableCell>
+                <TableCell align="right">Descripción</TableCell>
+                <TableCell align="right">Modificar</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {userinfo.articles.map((t) => (
+                <TableRow
+                  key={t.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                      {t.id}
+                  </TableCell>
+                  <TableCell align="right" sx={{ maxWidth: 900, overflow: 'hidden'}} >{t.title}</TableCell>
+                  <TableCell align="right">{t.description}</TableCell>
+                  <TableCell align="right"><Link to={`/backoffice/form/${t.id}`}>Editar curso</Link></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+            : <h4>¿Aún no te inscribiste a ningun curso? Conocé todos los que hay disponibles <a href='/curse'>aquí</a>  </h4>
 
 
         }
@@ -200,16 +358,38 @@ export default function VerticalTabs({ history }) {
       </TabPanel>
 
        <TabPanel value={value} index={3}>
-       Toda la informacion de las noticias
-         {userinfo.articles && userinfo.articles.map(t => {
-          return (
-            <div key={t.id} className={style.transactionDiv}>
-              <h6>{t.id}</h6>
-               <h6>{t.title}</h6>
-              <h6>{t.description}</h6>
-             </div>
-           )
-        })}
+            <h2>Editar y crear noticias </h2>
+            {
+              <TableContainer component={Paper} sx={{ maxHeight: 500 }}>
+              <Table sx={{ maxWidth: 1700, overflow: 'scroll'}} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>id</TableCell>
+                    
+                    <TableCell align="right">Título</TableCell>
+                    <TableCell align="right">Descripción</TableCell>
+                    <TableCell align="right">Modificar</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {news.map((t) => (
+                    <TableRow
+                      key={t.id}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                          {t.id}
+                      </TableCell>
+                      <TableCell align="right" sx={{ maxWidth: 900, overflow: 'hidden'}} >{t.title}</TableCell>
+                      <TableCell align="right">{t.description}</TableCell>
+                      <TableCell align="right"><Link to={`/backoffice/form/${t.id}`}>Editar noticia</Link></TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            
+            }
        </TabPanel>
       
     </Box>
